88e7e17747c625fb6e6baf8bac0004ee7c065cbd
Jivak Sute <jivakysute@gmail.com>
2023-04-28 12:52:18 +0530

    cicd

---
+++ index.js
@@ -0,0 +1,13 @@
+const str = "hello";
+
+console.log(str);
+
+const str2 = "world";
+
+console.log(str + " " + str2);
+
+if (str.length > str2.length) {
+  console.log("trueeeeeeee");
+} else {
+  console.log("false.....");
+}
